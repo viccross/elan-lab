300e76c32e45184d2032357a85b5fa2b5c31924e
Vic Cross <viccross@au.ibm.com>
2024-02-18 01:35:03 +0000

    Fixed logic test in destroy verification

--- zvm-lab.sh
+++ zvm-lab.sh
@@ -26,14 +26,14 @@ usage() {
 startall() {
 	for num in $(seq 1 3); do
 		chvm -a lxelan0${num}
-		sleep 3
+		sleep 2
 	done
 }
 
 stopall() {
 	for num in $(seq 1 3); do
 		chvm -d lxelan0${num}
-		sleep 3
+		sleep 1
 	done
 }
 
@@ -103,14 +103,14 @@ case $1 in
 			setall CMS
 			for num in $(seq 1 3); do
 				echo "destroyed" > .lxelan0${num}
-				while ! lsvm -s lxelan0${num} >/dev/null; do sleep 2; done
+				while lsvm -s lxelan0${num} >/dev/null; do sleep 1; done
 				echo "lxelan0${num} destroyed."
 			done
 		else
 			chvm -d $2
 			chvmipl -i CMS $2
 			echo "destroyed" > .$2
-			while ! lsvm -s $2 >/dev/null; do sleep 2; done
+			while lsvm -s $2 >/dev/null; do sleep 1; done
 			echo "$2 destroyed."
 		fi
 	;;
